df8eaf354f79767c5398f70de9c18e7cbf066631
Jonah <amittai@techpandainc.com>
2016-11-18 15:47:14 +0000

    [linux32_strcmp] Commit #0002

---
+++ linux32/libs/game.asm
@@ -0,0 +1,12 @@
+; Game code
+; Linux 32 bit
+
+;-----------------------------------
+;   int nameEasterEgg()
+;-----------------------------------
+nameEasterEgg:
+    mov eax, name
+    mov ebx, egg_name
+    call strEquals
+    
+    ret
--- linux32/main.asm
+++ linux32/main.asm
@@ -2,6 +2,7 @@
 ; Linux 32 bit
 
 %include "libs/stdlib.asm"
+%include "libs/game.asm"
 
 SECTION .text
 global _start
@@ -14,6 +15,10 @@ _start:
     mov ebx, 256
     call gets
     
+    call nameEasterEgg
+    cmp edx, 1
+    je .nameEgg
+    
     mov eax, hi
     call sprint
     
@@ -23,9 +28,21 @@ _start:
     mov eax, 0
     call exit
     
+.nameEgg:
+    mov eax, egg_name_yes
+    call sprint
+    
+    mov eax, 69
+    call exit
+    
 SECTION .data
 msg: db "What is your name: ",0
 hi: db "Hello ",0
+
+egg_name: db "Bob Saggit",10,0
+egg_name_yes: db "DUDE YES",10,0
+
+dbg_err: db "Error!",10,0
 
 SECTION .bss
 name: resb 256
